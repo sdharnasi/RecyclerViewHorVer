62ba49036cd6e1a2c36f274266cc514eb58af0d6
srinivasarao <srinivasaraouesi@gmail.com>
2017-12-27 19:23:29 +0530

    first commit

---
+++ app/src/main/java/com/srinu/recyclerviewhorver/adapter/MainAdapter.java
@@ -0,0 +1,117 @@
+package com.srinu.recyclerviewhorver.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.srinu.recyclerviewhorver.R;
+import com.srinu.recyclerviewhorver.SingleHorizontal;
+import com.srinu.recyclerviewhorver.SingleVertical;
+
+import java.util.ArrayList;
+
+import static com.srinu.recyclerviewhorver.MainActivity.getHorizontalData;
+import static com.srinu.recyclerviewhorver.MainActivity.getVerticalData;
+
+/**
+ * Created by ConquerorsTechnologies on 15/12/17.
+ */
+
+public class MainAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private Context context;
+    private ArrayList<Object> items;
+    private final int VERTICAL = 1;
+    private final int HORIZONTAL = 2;
+
+    public MainAdapter(Context context, ArrayList<Object> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view;
+        RecyclerView.ViewHolder holder;
+        switch (viewType) {
+            case VERTICAL:
+                view = inflater.inflate(R.layout.vertical, parent, false);
+                holder = new VerticalViewHolder(view);
+                break;
+            case HORIZONTAL:
+                view = inflater.inflate(R.layout.horizonatal, parent, false);
+                holder = new HorizontalViewHolder(view);
+                break;
+
+            default:
+                view = inflater.inflate(R.layout.horizonatal, parent, false);
+                holder = new HorizontalViewHolder(view);
+                break;
+        }
+
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (holder.getItemViewType() == VERTICAL)
+            verticalView((VerticalViewHolder) holder);
+        else if (holder.getItemViewType() == HORIZONTAL)
+            horizontalView((HorizontalViewHolder) holder);
+    }
+
+    private void verticalView(VerticalViewHolder holder) {
+
+        VerticalAdapter adapter1 = new VerticalAdapter(getVerticalData());
+        holder.recyclerView.setLayoutManager(new LinearLayoutManager(context));
+        holder.recyclerView.setAdapter(adapter1);
+    }
+
+
+    private void horizontalView(HorizontalViewHolder holder) {
+        HorizontalAdapter adapter = new HorizontalAdapter(getHorizontalData());
+        holder.recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+        holder.recyclerView.setAdapter(adapter);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (items.get(position) instanceof SingleVertical)
+            return VERTICAL;
+        if (items.get(position) instanceof SingleHorizontal)
+            return HORIZONTAL;
+        return -1;
+    }
+
+    public class HorizontalViewHolder extends RecyclerView.ViewHolder {
+
+        RecyclerView recyclerView;
+
+        HorizontalViewHolder(View itemView) {
+            super(itemView);
+            recyclerView = (RecyclerView) itemView.findViewById(R.id.inner_recyclerView);
+        }
+    }
+
+    public class VerticalViewHolder extends RecyclerView.ViewHolder {
+        RecyclerView recyclerView;
+
+        VerticalViewHolder(View itemView) {
+            super(itemView);
+            recyclerView = (RecyclerView) itemView.findViewById(R.id.inner_recyclerView);
+        }
+    }
+
+}
+
